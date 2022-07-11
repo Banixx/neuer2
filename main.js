3c5bc2888a72809db7eed1f6ea45113e44ced141
Banixx <90242722+Banixx@users.noreply.github.com>
2022-07-11 17:24:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let go =  function(){
-hoi ="sali";
-let ele= document.getElementById("hoi");
-ele.innerHTML="Hellou";
+let funktiion = function () {
+  hoi = "sali";
+  let ele = document.getElementById("hoi");
+  ele.innerHTML = "Hellou";
+  console.log("knopf gedrückt");
+};
 
-}
+let funktiion2 = function () {
+  hoi2 = "sali2";
+  let ele = document.getElementById("hoi");
+  ele.innerHTML = "Hoi";
+  console.log("knopf gedrückt2");
+};
+
+console.log("huihuiui");
+knopf.addEventListener("click", funktiion);
+knopf2.addEventListener("click", funktiion2);
